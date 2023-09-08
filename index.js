2dbddae6d3f7f9cd76a35b81c28b714eeed3c32b
rajxode <rajsh1202@gmail.com>
2023-09-08 11:21:11 +0530

    create folder structure for project and fireup the server

---
+++ index.js
@@ -0,0 +1,14 @@
+
+require('dotenv').config();
+const express = require('express');
+
+const {PORT} = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running or Port: ${PORT}`);
+})
