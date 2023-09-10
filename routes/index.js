9cbe090670ca2f074ec0646572c14c49ad90630e
rajxode <rajsh1202@gmail.com>
2023-09-11 00:03:00 +0530

    add comments to routes file

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 
+// import express
 const express = require('express');
 
+// create router 
 const router = express.Router();
 
+// import home page Controller
 const homeController = require('../controllers/homeController');
 
-
+// render the homepage
 router.get('/',homeController.home);
 
+// import routes for products
 router.use('/products',require('./product'))
 
-
+// export the router
 module.exports = router;
