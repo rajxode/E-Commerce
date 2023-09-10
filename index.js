fb76f495d849777335ddbfd79ddbebb8b6854863
rajxode <rajsh1202@gmail.com>
2023-09-11 00:05:58 +0530

    Entry point, added comments to index.js file

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 
+// for environment variables
 require('dotenv').config();
+
+// connect to database
 require('./config/mongoose').connect();
+
+// import expressjs
 const express = require('express');
 
+// define the PORT from env variables
 const {PORT} = process.env;
 
+// create app
 const app = express();
 
+// for swagger api documentations
 const swaggerUi = require('swagger-ui-express');
+// import YAML for writing swagger docs
 const YAML = require('yamljs')
+// swagger file
 const swaggerDocument = YAML.load('./swagger.yaml')
 
+
+// middleware for reading json data
 app.use(express.json());
+// middleware for url data
 app.use(express.urlencoded({
     extended:true
 }));
+
+// middleware to render the swagger docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 // setting up routes
 app.use('/',require('./routes'));
 
+
+// start server
 app.listen(PORT, () => {
     console.log(`Server is running or Port: ${PORT}`);
 })
